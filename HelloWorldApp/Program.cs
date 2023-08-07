ec7bb6966516cfdd3fb826ce260eb4c46ad3770c
pawankhandwe <pawankhandwe@gmail.com>
2023-08-07 14:41:37 +0530

    Initial commit

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
