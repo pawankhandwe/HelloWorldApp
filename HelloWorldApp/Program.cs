dd0d03a2c83fb0fe8968fc5621b591218172f9a9
pawankhandwe <pawankhandwe@gmail.com>
2023-08-07 17:18:51 +0530

    Initial commit

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -3,10 +3,22 @@
 
 
 
-    class Program
+class Program
+{
+    static int Sum(int a, int b)
     {
-        static void Main(string[] args)
-        {
-            System.Console.WriteLine("Hello World!");
-        }
+        return a + b;
     }
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World!");
+
+        int num1 = 5;
+        int num2 = 7;
+        int result = Sum(num1, num2);
+
+        System.Console.WriteLine($"The sum of {num1} and {num2} is {result}");
+    }
+}
+
